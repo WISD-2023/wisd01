2e4aaac8002aa00cbe8efec0d26547e8501d45bf
3B032112 <rjoeyic@gmail.com>
2023-12-06 10:15:44 +0800

    ff.f2

--- routes/web.php
+++ routes/web.php
@@ -53,5 +53,8 @@ Route::get('/dd/d2', function () {
 Route::get('/ff/f1', function () {
     return view('ff.f1');
 });
+Route::get('/ff/f2', function () {
+    return view('ff.f2');
+});
 
 require __DIR__ . '/auth.php';
